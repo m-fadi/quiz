5676b23bd7ae6e695f6b0ac92f351e33b0781bd6
fadi <fadi.m.marouf@gmail.com>
2022-12-19 18:05:36 +0100

    sortQuestions: fix:bug: option id =index

--- client/src/utils/sortQuestions.js
+++ client/src/utils/sortQuestions.js
@@ -1,22 +1,22 @@
 import { nanoid } from "nanoid";
 export const sortQuestions = (questions) => {
-    return questions.map((question,index) => {
-        console.log("question", questions);
+    return questions.map((question) => {
+        //console.log("question", questions);
         const questionText = question.question;
-        const incorrectAnswers = question.incorrect_answers.map((answer) => ({
+        const incorrectAnswers = question.incorrect_answers.map((answer,index) => ({
             value: answer,
             isHeld: false,
             isCorrect: false,
             styles: { background: "white" },
             id:index,
         }));
-        console.log("incorrect in sort", incorrectAnswers);
+        //console.log("incorrect in sort", incorrectAnswers);
         const correctAnswer = {
             value: question.correct_answer,
             isHeld: false,
             isCorrect: true,
         };
-        console.log("correct in sort", correctAnswer);
+        //console.log("correct in sort", correctAnswer);
         const allAnswersArray = [...incorrectAnswers, correctAnswer].sort(
             () => Math.random() - 0.5
         );
