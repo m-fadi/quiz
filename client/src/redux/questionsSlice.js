cbc92788fa5ff97943b106b82002e0c1be938818
fadi <fadi.m.marouf@gmail.com>
2022-12-19 18:04:56 +0100

    quesSlice: add updateIsHeld

--- client/src/redux/questionsSlice.js
+++ client/src/redux/questionsSlice.js
@@ -1,4 +1,4 @@
-import { createSlice,current } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const initialState = [];
 
@@ -7,37 +7,74 @@ export const questionsSlice = createSlice({
     initialState,
     reducers: {
         getQuestions: (state, action) => {
-            console.log("state in quesSlice", state.questions);
-            console.log("payload in questionsSlice", action.payload);
-
-            state.push(...action.payload)
+            state.push(...action.payload);
         },
         updateIsHeld: (state, action) => {
             console.log("payload in updateIsHeld", action.payload);
-           
-            const question = current(state).filter((ques) => {
-                return ques.id === action.payload.quesId;
+            console.log("state in update: ", current(state));
+
+            const results = JSON.stringify(current(state));
+            const resultsParsed = JSON.parse(results);
+
+            const test = resultsParsed.map((question, index) => {
+                if (question.id === action.payload.quesId) {
+                    question.allAnswers[action.payload.optionId].isHeld =
+                        !question.allAnswers[action.payload.optionId].isHeld;
+                    console.log(question.allAnswers);
+                }
+
+                return question;
             });
-            console.log("the clicked question", question);
-            // state.map((ques) => {
-            //     if (ques.id === action.payload.quesId) {
-            //         console.log("the clicked question", ques);
-            //         ques.allAnswers.map((option) => {
-            //             if (option.id === action.payload.optionId) {
-            //                 console.log("the clicked qoption", option);
-            //                 return {
-            //                     ...option,
-            //                     isHeld: true,
-            //                     styles: { background: "lightblue" },
-            //                 };
-            //             } else return option;
-            //         });
-            //     }
-            //     return state.questions;
-            // });
+
+            console.log("TEST: ", test);
+            return [...test];
         },
     },
 });
-export const { getQuestions,updateIsHeld } = questionsSlice.actions;
+export const { getQuestions, updateIsHeld } = questionsSlice.actions;
 
 export default questionsSlice.reducer;
+
+// console.log("options", option);
+// console.log("optionId from payload", action.payload.optionId);
+// console.log("the clicked question", ques);
+
+// updateIsHeld: (state, action) => {
+//     // console.log("payload in updateIsHeld", action.payload);
+
+//     // const question = current(state).filter((ques) => {
+//     //     return ques.id === action.payload.quesId;
+//     // });
+//     // console.log("the clicked question", question[0]);
+//     // console.log(
+//     //     "isHeld",
+//     //     (question[0].allAnswers[action.payload.optionId].isHeld)
+//     // );
+//     // question[0].allAnswers[action.payload.optionId].isHeld = true;
+
+//     //console.log("the clicked question", question[0]);
+
+//     current(state).map((ques) => {
+//         if (ques.id === action.payload.quesId) {
+
+//             const opt=ques.allAnswers.map((option) => {
+
+//                 if (option.id == action.payload.optionId) {
+//                     console.log("the clicked qoption", option);
+//                     return {
+//                         ...option,
+//                         isHeld: true,
+//                         styles: { background: "lightblue" },
+//                     };
+//                 } else {
+//                     console.log("wrong option", option.id);
+//                     return option;
+//                 }
+//             });
+//         }
+
+//         return [...ques, ];
+//     });
+//     console.log(current(state));
+//     return [...state]
+// },
