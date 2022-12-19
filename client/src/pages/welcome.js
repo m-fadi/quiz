3acee11835c664b25bd1d7c6174552570c80b2ea
fadi <fadi.m.marouf@gmail.com>
2022-12-19 18:09:36 +0100

    clean up

--- client/src/pages/welcome.js
+++ client/src/pages/welcome.js
@@ -1,22 +1,31 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Registration from "./Registration"
-import Home from "./Home"
+import Registration from "./Registration";
+import Home from "./Home";
 //import Login from "./login";
 //import ResetPassword from "./resetPassword";
 
 export default function Welcome() {
     return (
         <div id="welcome">
-            
-
             {/* <img className="logo-img" src="../images/logo.jpeg" /> */}
             <div>
                 <BrowserRouter>
                     <Routes>
-                        <Route exact path="/" element={<Registration />}></Route>
-                        <Route path="/register" element={<Registration />}></Route>
-                         <Route path="/Home" element={<Home />}></Route> 
+                        <Route
+                            exact
+                            path="/"
+                            element={<Registration />}
+                        ></Route>
+                        <Route
+                            path="/register"
+                            element={<Registration />}
+                        ></Route>
+                        <Route
+                            path="/
+                         "
+                            element={<Home />}
+                        ></Route>
                         {/* <Route
                             path="/resetPassword"
                             element={<ResetPassword />}
