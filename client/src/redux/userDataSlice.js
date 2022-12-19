aa333b923ec16ddee900b4f1914660ab827b8c22
fadi <fadi.m.marouf@gmail.com>
2022-12-19 18:09:57 +0100

    add userDataSlice for registration data

---
+++ client/src/redux/userDataSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password:"",
+    
+};
+export const quizSlice = createSlice({
+    name: "userData",
+    initialState,
+    reducers: {
+        handleFirstName: (state, action) => {
+            console.log("SSSSlice", action.payload);
+            return { ...state, firstName: action.payload };
+        },
+        handleLastName: (state, action) => {
+            console.log("SSSSlice diff", action.payload);
+            return { ...state, lastName: action.payload.level };
+        },
+        handleEmail: (state, action) => {
+            console.log("SSSSlice type", action.payload);
+            return { ...state, email: action.payload.type };
+        },
+        handlePassword: (state, action) => {
+            console.log("SSSSlice number", action.payload);
+            return { ...state, password: action.payload };
+        },
+       
+    },
+});
+export const {
+    handleFirstName,
+    handleLastName,
+    handleEmail,
+    handlePassword,
+} = quizSlice.actions;
+
+export default quizSlice.reducer;
