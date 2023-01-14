5e7e960512d531479bca45d5c78f5b15e2b48b1b
fadi <fadi.m.marouf@gmail.com>
2023-01-14 20:16:53 +0100

    add function createUser

---
+++ client/src/utils/createUser.js
@@ -0,0 +1,20 @@
+const createUser = async(data) => {
+    console.log(data);
+    const response= await fetch("/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ data }),
+    })
+    const result= await response.json()
+    
+        // .then((result) => result.json())
+
+        // .then((result) => {
+             console.log("results in createUser", result.result);
+
+            return result.result;
+
+        //     //location.reload();
+        // });
+};
+export default createUser;
