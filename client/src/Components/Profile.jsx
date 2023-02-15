749939c74bacb5f1133811ea4d1e281fccbf46ca
fadi <fadi.m.marouf@gmail.com>
2023-02-15 08:54:55 +0100

    add profile component

---
+++ client/src/Components/Profile.jsx
@@ -0,0 +1,25 @@
+import {
+    Typography,
+    AppBar,
+    Card,
+    CardAction,
+    CardContent,
+    CardMedia,
+    CssBaseline,
+    Grid,
+    Toolbar,
+    container,
+    Avatar,
+    
+} from "@mui/material";
+const Profile = () => {
+    return (
+        <>
+            <Avatar>H</Avatar>
+            <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar>
+            <Avatar sx={{ bgcolor: deepPurple[500] }}>OP</Avatar>
+        </>
+    );
+};
+
+export default Profile;
